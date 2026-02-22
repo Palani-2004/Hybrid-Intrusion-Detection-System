5544cc36f8ca0798495de3b59c0579ab4acea608
Palani <donpalani1594@gmail.com>
2026-02-22 18:17:30 +0530

    Fix apps.py crash

--- nids_app/apps.py
+++ nids_app/apps.py
@@ -1,17 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import User
+
 
 class NidsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nids_app'
-
-    def ready(self):
-        try:
-            if not User.objects.filter(username="admin").exists():
-                User.objects.create_superuser(
-                    username="admin",
-                    email="admin@example.com",
-                    password="admin123"
-                )
-        except:
-            pass
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "nids_app"
